3aec1776dfff191b4c0d46fdbc092af4d4ff5c03
anthony sanna <anthonysanna304@gmail.com>
2022-05-05 23:14:16 +0200

    [FIX] regex

--- src/components/Regex.js
+++ src/components/Regex.js
@@ -3,12 +3,12 @@
 export const validEmail = new RegExp('^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$');
 
 //export const validPassword = new RegExp('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$');
-export const validPassword = new RegExp('[A-Za-z1-9]');
+export const validPassword = new RegExp('[A-Za-z0-9]');
 
-export const validPseudo = new RegExp('^[A-Za-z1-9_-]+$');
+export const validPseudo = new RegExp('^[A-Za-z0-9_-]+$');
 
-export const validPost = new RegExp('^[-A-Za-z1-9_" ,()é!àç:.?\']+$');
+export const validPost = new RegExp('^[-A-Za-z0-9_" ,()é!àç:.?\']+$');
 
-export const validComment = new RegExp('^[-A-Za-z1-9_" ,()é!àç:.?\']+$');
+export const validComment = new RegExp('^[-A-Za-z0-9_" ,()é!àç:.?\']+$');
 
 
